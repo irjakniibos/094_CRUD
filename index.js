be917fdee03df563dbc34d462bb1aae5e8978985
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-20 10:04:18 +0700

    Membuat method POST

--- index.js
+++ index.js
@@ -43,3 +43,25 @@ app.get('/api/users', (req, res) => {
     });
 });
 
+app.post('/api/users', (req, res) => {
+    const { nama, nim, kelas } = req.body;
+
+    if(!nama || !nim || !kelas) {
+        return  res.status(400).send({'message': 'nama, nim, dan kelas harus diisi'});
+    }
+
+    db.query(
+        'INSERT INTO mahasiswa (nama, nim, kelas) VALUES (?, ?, ?)',
+        [nama, nim, kelas],
+        (err, result) => {
+
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+            res.status(201).json({ message: 'User added successfully' });
+        }
+    );
+});
+
+
